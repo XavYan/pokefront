4b45004d1961084e5cf126e4b8278c966fe2ccb8
XavYan <xavieryanquin@gmail.com>
2021-04-12 19:38:10 +0100

    Installed router and migrated App into Home

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,34 @@
 <template>
   <div class="header">
-    <logo />
+    <router-link
+      class="logo-container"
+      to="/"
+    >
+      <pokefront-logo />
+    </router-link>
+    <div id="nav">
+      <router-link
+        to="/search"
+        active-class="nav-active"
+      >
+        Search
+      </router-link>
+      <router-link
+        to="/about"
+        active-class="nav-active"
+      >
+        About
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-import Logo from './Logo.vue'
+import PokefrontLogo from './PokefrontLogo.vue'
 
 export default {
   name: 'Header',
-  components: { Logo }
+  components: { PokefrontLogo }
 }
 </script>
 
@@ -22,5 +41,36 @@ export default {
   padding: 0 50px;
 
   color: #1D3557;
+
+  .logo-container {
+    text-decoration: none;
+  }
+
+  #nav {
+    width: 100%;
+    margin-left: 50px;
+
+    :nth-child(n) {
+      color: white;
+      text-decoration: none;
+      padding: 0 10px;
+      height: 100%;
+      font-size: 20px;
+      margin: 0 3px;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &:last-child {
+        margin-right: 0;
+      }
+
+      &:hover, &.nav-active {
+        color: #F4A261;
+        // border-bottom: 2px solid #F4A261;
+      }
+    }
+  }
 }
 </style>
